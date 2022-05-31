6699be6f7608f3173cb8edd8341f7a602df080f9
Nathan Nicholson <85798301+TheNathanN@users.noreply.github.com>
2022-05-30 20:46:37 -0500

    Add assets to public folder & write basic set up

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    colors: {
+      white: '#fff',
+      black: '#000',
+      darkBlue: '#18172F',
+      blue: '#0B0D48',
+    },
+    fontFamily: {
+      sans: ['Oswald', 'sans-serif'],
+    },
+    extend: {
+      backgroundImage: {
+        'gradient-radial':
+          'radial-gradient(var(--tw-gradient-to), var(--tw-gradient-from))',
+      },
+    },
   },
   plugins: [],
-}
+};
