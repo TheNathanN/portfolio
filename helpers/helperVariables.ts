0db0b36d904a973742638c7fe5a1a46031712c4a
Nathan Nicholson <85798301+TheNathanN@users.noreply.github.com>
2022-06-29 16:50:16 -0500

    Update labels

--- helpers/helperVariables.ts
+++ helpers/helperVariables.ts
@@ -1,60 +1,60 @@
 export const _NavItems = [
   {
-    label: 'Tech',
-    link: '#tech',
+    label: "Tech",
+    link: "#tech",
   },
   {
-    label: 'Portfolio',
-    link: '#portfolio',
+    label: "Portfolio",
+    link: "#portfolio",
   },
   {
-    label: 'Contact',
-    link: '#contact',
+    label: "Contact",
+    link: "#contact",
   },
 ];
 
 export const _TechLabels = [
-  'HTML',
-  'CSS / SCSS',
-  'Tailwind CSS',
-  'Framer Motion',
-  'Typescript',
-  'React / Redux',
-  'Next JS',
-  'Node JS',
-  'MySQL',
-  'Jest Testing',
+  "HTML",
+  "CSS / SCSS",
+  "Tailwind CSS",
+  "Framer Motion",
+  "Typescript",
+  "React / Redux",
+  "Next.js",
+  "Node.js",
+  "SQL",
+  "Jest Testing",
 ];
 
 export const _CryptoTrackerLabels = [
-  'HTML / SCSS',
-  'Next JS',
-  'Chart JS',
-  'Context API',
-  'Coinranking API',
-  'Bing API',
-  'Next JS Proxy Server for API Requests',
+  "HTML / SCSS",
+  "Next.js",
+  "Chart.js",
+  "Context API",
+  "Coinranking API",
+  "Bing API",
+  "Next JS Proxy Server for API Requests",
 ];
 
 export const _LofiRadioLabels = [
-  'HTML / SCSS',
-  'Next JS',
-  'Context API',
-  'Framer Motion',
+  "HTML / SCSS",
+  "Next.js",
+  "Context API",
+  "Framer Motion",
 ];
 
 export const _PayApiLabels = [
-  'HTML / Tailwind CSS',
-  'Typescript',
-  'React JS',
-  'Redux',
-  'Express JS',
-  'MySQL',
+  "HTML / Tailwind CSS",
+  "Typescript",
+  "React.js",
+  "Redux",
+  "Express.js",
+  "MySQL",
 ];
 
 export const _PomodoroLabels = [
-  'HTML / SCSS',
-  'Typescript',
-  'Next JS',
-  'Context API',
+  "HTML / SCSS",
+  "Typescript",
+  "Next.js",
+  "Context API",
 ];
