289ba2cead7c84666b9bb027d5c162a37ccd61c1
Nathan Nicholson <85798301+TheNathanN@users.noreply.github.com>
2022-09-27 09:19:52 -0700

    Fix contact confirmation message

--- components/contact/contact.tsx
+++ components/contact/contact.tsx
@@ -1,11 +1,11 @@
-import { useEffect } from 'react';
-import { motion, useAnimation } from 'framer-motion';
-import { useInView } from 'react-intersection-observer';
-import { useForm, ValidationError } from '@formspree/react';
-import ShadowSVG from '../svg/shadow';
+import { useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useForm, ValidationError } from "@formspree/react";
+import ShadowSVG from "../svg/shadow";
 
 const Contact = () => {
-  const [state, handleSubmit] = useForm('mrgjdgbg');
+  const [state, handleSubmit] = useForm("mrgjdgbg");
   const animation = useAnimation();
 
   const { ref, inView } = useInView({
@@ -25,15 +25,15 @@ const Contact = () => {
   if (state.succeeded) {
     return (
       <>
-        <div className='w-full h-full absolute top-0'>
+        <div className="w-full h-full absolute top-0">
           <ShadowSVG />
         </div>
 
-        <h2 className='font-bold text-center relative text-5xl mt-24 mb-11 lg:text-6xl lg:mb-20'>
+        <h2 className="font-bold text-center relative text-5xl mt-24 mb-11 lg:text-6xl lg:mb-20">
           Contact
         </h2>
 
-        <p className='relative flex flex-col text-center justify-center items-center w-10/12 max-w-xl h-72 mb-16 text-lg md:text-xl lg:mb-36'>
+        <p className="relative flex flex-col text-center justify-center items-center w-full h-72 mb-16 text-lg md:text-xl lg:mb-36">
           The form has been submitted successfully.
           <br />
           I&#39;m looking forward to furthering our communication!
@@ -45,16 +45,16 @@ const Contact = () => {
   return (
     <div
       ref={ref}
-      className='relative w-full flex flex-col items-center md:mt-8'
+      className="relative w-full flex flex-col items-center md:mt-8"
     >
-      <div className='w-full h-full absolute top-0'>
+      <div className="w-full h-full absolute top-0">
         <ShadowSVG />
       </div>
 
       <motion.h2
         initial={{ opacity: 0, y: 100 }}
         animate={animation}
-        className='font-bold text-center relative text-5xl mt-24 mb-11 lg:text-6xl lg:mb-20'
+        className="font-bold text-center relative text-5xl mt-24 mb-11 lg:text-6xl lg:mb-20"
       >
         Contact
       </motion.h2>
@@ -63,48 +63,48 @@ const Contact = () => {
         initial={{ opacity: 0, y: 100 }}
         animate={animation}
         onSubmit={handleSubmit}
-        className='relative flex flex-col w-11/12 max-w-xl text-base lg:text-xl'
+        className="relative flex flex-col w-11/12 max-w-xl text-base lg:text-xl"
       >
-        <label htmlFor='name'>Name</label>
+        <label htmlFor="name">Name</label>
         <input
-          className='my-4 p-2 rounded-sm text-black outline-none'
-          id='name'
-          name='name'
-          type='text'
+          className="my-4 p-2 rounded-sm text-black outline-none"
+          id="name"
+          name="name"
+          type="text"
           required
         />
 
-        <ValidationError prefix='Name' field='name' errors={state.errors} />
+        <ValidationError prefix="Name" field="name" errors={state.errors} />
 
-        <label htmlFor='email'>Email</label>
+        <label htmlFor="email">Email</label>
         <input
-          className='my-4 p-2 rounded-sm text-black outline-none'
-          id='email'
-          name='email'
-          type='email'
+          className="my-4 p-2 rounded-sm text-black outline-none"
+          id="email"
+          name="email"
+          type="email"
           required
         />
 
-        <ValidationError prefix='Email' field='email' errors={state.errors} />
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
 
-        <label htmlFor='message'>Message</label>
+        <label htmlFor="message">Message</label>
         <textarea
-          className='my-4 p-2 rounded-sm text-black outline-none'
-          name='message'
-          id='message'
+          className="my-4 p-2 rounded-sm text-black outline-none"
+          name="message"
+          id="message"
           cols={5}
           rows={8}
         ></textarea>
 
         <ValidationError
-          prefix='Message'
-          field='message'
+          prefix="Message"
+          field="message"
           errors={state.errors}
         />
 
         <button
-          className='bg-white text-black w-36 py-2 mx-auto mt-9 mb-14 rounded-sm cursor-pointer hover:bg-lightBlue hover:text-white transition-all duration-100 ease-in'
-          type='submit'
+          className="bg-white text-black w-36 py-2 mx-auto mt-9 mb-14 rounded-sm cursor-pointer hover:bg-lightBlue hover:text-white transition-all duration-100 ease-in"
+          type="submit"
           disabled={state.submitting}
         >
           Submit
